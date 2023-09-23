da1e7e117648d6a56ee83f5a317c884aeb648053
Carlos Silva <Carlos.TavaresdaSilva@werum.com>
2023-09-23 21:26:00 +0200

    Update the route import

--- pgimporter_app/views.py
+++ pgimporter_app/views.py
@@ -10,13 +10,33 @@ from . import app
 def about():
     return render_template("about.html")
 
-@app.route("/import/", methods=['GET','POST'])
+@app.route("/import", methods=['GET','POST'])
 def dashboardimport():
-    items = Pgimporter.read_dumps('/u02/pgbackup/db/postgres/import/')
+    
     if request.method == 'GET':
-        return render_template("import.html", result='<div class="alert" role="alert"> Choose a dump to import.</div>', items=items)
+        import_sql_files = Pgimporter.read_dumps('/u02/pgbackup/db/postgres/import/')
+        import_logs = Pgimporter.read_dumps('/u02/pgbackup/db/postgres/import_logs/')
+        return render_template("import.html", result='<div class="alert alert-secondary" role="alert"> Choose a dump to import.</div>', import_sql_files=import_sql_files, import_logs=import_logs)
     if request.method == 'POST':
-        return render_template("import.html", result='<div class="alert" role="alert"> Choose a dump to import.</div>', items=items)
+        #if 'file' not in request.files:
+        #   result='<div class="alert alert-warning" role="alert">No file provided!</div>'
+    
+        file = request.files['file']
+    
+        if file.filename == '': 
+            result='<div class="alert alert-warning" role="alert">No file selected.</div>'
+        elif file:
+            try:
+                filename = secure_filename(file.filename)
+                file.save(os.path.join('/u02/pgbackup/db/postgres/import', filename))
+                result='<div class="alert alert-success" role="alert">File uploaded successfully.</div>'
+            except OSError as e:
+                result='<div class="alert alert-danger" role="alert">Unexpected Error on upload:'+ e.strerror +'</div>' 
+        
+        import_sql_files = Pgimporter.read_dumps('/u02/pgbackup/db/postgres/import/')
+        import_logs = Pgimporter.read_dumps('/u02/pgbackup/db/postgres/import_logs/')
+
+        return render_template("import.html", result=result, import_sql_files=import_sql_files, import_logs=import_logs)
 
 #items = ["Item 1", "Item 2", "Item 3"]
 
@@ -29,32 +49,39 @@ def dashboarddumps():
 @app.route('/download/<filename>')
 def download_file(filename):
     
-    # Define the path to the file you want to serve
-    file_path = f'/u02/pgbackup/db/postgres/dumps/{filename}'  # Replace with the actual path to your files
-
+    if filename.split('.')[1] == 'sql':
+        # Define the path to the file you want to serve
+        file_path = f'/u02/pgbackup/db/postgres/dumps/{filename}'  # Replace with the actual path to your files
+    elif filename.split('.')[1] == 'log':
+        # Define the path to the file you want to serve
+        file_path = f'/u02/pgbackup/db/postgres/import_logs/{filename}'  # Replace with the actual path to your files
     # Use send_file to send the file to the user's browser
     return send_file(file_path, as_attachment=True)
 
-@app.route('/upload', methods=['GET','POST'])
-def upload_file():
-    if 'file' not in request.files:
-        return render_template("import.html", result='<div class="alert alert-warning" role="alert">No file provided!</div>')
-    
-    file = request.files['file']
-    
-    if file.filename == '': return render_template("import.html", result='<div class="alert alert-warning" role="alert">No file selected.</div>')
-    
-    if file:
-        filename = secure_filename(file.filename)
-        file.save(os.path.join('/u02/pgbackup/db/postgres/import', filename))
-        result='<div class="alert alert-success" role="alert">File uploaded successfully.</div>'
-        return render_template("import.html", result=result)
+
+#TO DELETE AFTER TESTING
+#@app.route('/upload', methods=['GET','POST'])
+#def upload_file():
+#    if 'file' not in request.files:
+#        return render_template("import.html", result='<div class="alert alert-warning" role="alert">No file provided!</div>')
+#    
+#    file = request.files['file']
+#    
+#    if file.filename == '': return render_template("import.html", result='<div class="alert alert-warning" role="alert">No file selected.</div>')
+#    
+#    if file:
+#        filename = secure_filename(file.filename)
+#        file.save(os.path.join('/u02/pgbackup/db/postgres/import', filename))
+#        result='<div class="alert alert-success" role="alert">File uploaded successfully.</div>'
+#        return render_template("import.html", result=result)
 
 @app.route('/import/<filename>')
 def import_dump(filename):
     #pass the filename to method import_dump() of pgimporter
     result = Pgimporter.import_dump(filename)
-    return render_template("import.html", result=result)
+    import_sql_files = Pgimporter.read_dumps('/u02/pgbackup/db/postgres/import/')
+    import_logs = Pgimporter.read_dumps('/u02/pgbackup/db/postgres/import_logs/')
+    return render_template("import.html", result=None, import_sql_files=import_sql_files, import_logs=import_logs)
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0')
+#if __name__ == "__main__":
+#    app.run(host='0.0.0.0')
