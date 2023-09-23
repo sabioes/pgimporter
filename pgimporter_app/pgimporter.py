0b7eea6af956abefdbd718df32a352d792007e91
Carlos Silva <Carlos.TavaresdaSilva@werum.com>
2023-09-23 17:44:35 +0200

    new wave of implementations

--- pgimporter_app/pgimporter.py
+++ pgimporter_app/pgimporter.py
@@ -1,11 +1,8 @@
-import os
+import os, datetime, subprocess
 
 class Pgimporter:
     @staticmethod
-    def read_imports():
-
-        # Specify the path to the folder you want to analyze
-        folder_path = 'C:\Workspace\Learn Projects\pgimporter\pgimporter_app'
+    def read_dumps(folder_path):
 
         # Initialize an empty list to store file information
         file_info_list = []
@@ -16,14 +13,55 @@ class Pgimporter:
 
             # Check if the item is a file (not a directory)
             if os.path.isfile(file_path):
+                
+                file_type_bs_color = "info" if filename.split(".")[1] == "sql" else "primary"
+                file_type = '<span class="badge bg-'+ file_type_bs_color +'">'+filename.split(".")[1]+'</span>'
+                
                 # Get the size of the file in bytes
                 file_size = os.path.getsize(file_path)
+                
+                # Extract the creation timestamp (ctime) from the file_stat
+                creation_timestamp = os.stat(file_path).st_ctime
+
+                # Convert the timestamp to a human-readable date format
+                file_creation_date = datetime.datetime.fromtimestamp(creation_timestamp).strftime('%Y-%m-%d %H:%M:%S')
 
                 # Append the filename and its size to the list
-                file_info_list.append((filename, file_size))
+                file_info_list.append((file_type, filename, file_size, file_creation_date))
 
         # Now, file_info_list contains tuples of (filename, file_size)
         # You can use this list to access the file names and their sizes
         #for filename, file_size in file_info_list:
         #    print(f"File: {filename}, Size: {file_size} bytes")
         return file_info_list
+    
+    @staticmethod
+    def import_dump(dump_filename):
+        #https://stackoverflow.com/questions/43380273/pg-dump-pg-restore-password-using-python-module-subprocess
+        #Run the OS level command "pg_dump postgres"
+        if dump_filename.split(".")[1] != "sql": return "Incorrect dump file: " + dump_filename.split(".")[1]
+
+
+        # Replace 'command' with the actual command you want to run
+        absdumppath = '/u02/pgbackup/db/postgres/import/' + dump_filename
+        
+        database = 'postgres'
+        log = 'pg_dump_result.log' 
+        #pg_dump postgres --verbose > ats110-after-qf.sql | tee pg_dump_ats110_after_qf_execution.log
+        
+        command = 'pg_dump ' + database + ' --verbose > ' + absdumppath 
+        uid = 54321
+        
+        try:
+            print("Running the pg_dump command: " + command)
+            result = subprocess.run(command, check=True, shell=True, preexec_fn=lambda: os.setuid(uid), stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            
+            result_file = open('/u02/pgbackup/db/postgres/import/pg_dump_result_'+ datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S') + '.log', 'w')
+            result_file.write(str(result.stderr))
+            result_file.close()
+            
+            if result.returncode == 0 : return "Import Succeeded!" 
+            else: return "Import not Succeeded!"
+        except subprocess.CalledProcessError as e:
+            # Handle errors
+            return(f"Command failed with error: {e.returncode}\n{e.output}")
--- pgimporter_app/views.py
+++ pgimporter_app/views.py
@@ -1,39 +1,57 @@
 from flask import Flask
-from flask import render_template
-from datetime import datetime
+from flask import render_template, send_file, request
+from werkzeug.utils import secure_filename
+import os
 
 from pgimporter_app.pgimporter import Pgimporter
 from . import app
-
-@app.route("/")
-def home():
-    return render_template("home.html")
 
 @app.route("/about/")
 def about():
     return render_template("about.html")
 
-@app.route("/contact/")
-def contact():
-    return render_template("contact.html")
+@app.route("/import/", methods=['GET','POST'])
+def dashboardimport():
+    items = Pgimporter.read_dumps('/u02/pgbackup/db/postgres/import/')
+    if request.method == 'GET':
+        return render_template("import.html", result='<div class="alert" role="alert"> Choose a dump to import.</div>', items=items)
+    if request.method == 'POST':
+        return render_template("import.html", result='<div class="alert" role="alert"> Choose a dump to import.</div>', items=items)
 
 #items = ["Item 1", "Item 2", "Item 3"]
 
-@app.route("/pgimporter/")
-def dashboard():
+@app.route("/")
+def dashboarddumps():
     
-    items = Pgimporter.read_imports()
-    return render_template("dashboard.html", items=items)
+    items = Pgimporter.read_dumps('/u02/pgbackup/db/postgres/dumps/')
+    return render_template('dumps.html', items=items)
 
-@app.route("/hello/")
-@app.route("/hello/<name>")
-def hello_there(name = None):
-    return render_template(
-        "hello_there.html",
-        name=name,
-        date=datetime.now()
-    )
+@app.route('/download/<filename>')
+def download_file(filename):
+    
+    # Define the path to the file you want to serve
+    file_path = f'/u02/pgbackup/db/postgres/dumps/{filename}'  # Replace with the actual path to your files
 
-@app.route("/api/data")
-def get_data():
-    return app.send_static_file("data.json")
+    # Use send_file to send the file to the user's browser
+    return send_file(file_path, as_attachment=True)
+
+@app.route('/upload', methods=['GET','POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return render_template("import.html", result='<div class="alert alert-warning" role="alert">No file provided!</div>')
+    
+    file = request.files['file']
+    
+    if file.filename == '': return render_template("import.html", result='<div class="alert alert-warning" role="alert">No file selected.</div>')
+    
+    if file:
+        filename = secure_filename(file.filename)
+        file.save(os.path.join('/u02/pgbackup/db/postgres/import', filename))
+        result='<div class="alert alert-success" role="alert">File uploaded successfully.</div>'
+        return render_template("import.html", result=result)
+
+@app.route('/import/<filename>')
+def import_dump(filename):
+    #pass the filename to method import_dump() of pgimporter
+    result = Pgimporter.import_dump(filename)
+    return render_template("import.html", result=result)
