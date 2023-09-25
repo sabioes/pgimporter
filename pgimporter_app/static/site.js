6c2964c06823990d31676bc68dcaf5262044743d
Carlos Silva <Carlos.TavaresdaSilva@werum.com>
2023-09-26 00:32:25 +0200

    update js adding better query selector of navbar

--- pgimporter_app/static/site.js
+++ pgimporter_app/static/site.js
@@ -1,8 +1,17 @@
-(function () {
-    'use strict'
-  
-    document.querySelector('#navbarSideCollapse').addEventListener('click', function () {
+$(document).ready(function(){
+  'use strict'
+
+  const element = document.querySelector('#navbarSideCollapse');
+  if (element) {
+    element.addEventListener('click', function (){
       document.querySelector('.offcanvas-collapse').classList.toggle('open')
-    })
-  })()
-  
+    });
+  }
+
+});
+
+
+
+
+
+
