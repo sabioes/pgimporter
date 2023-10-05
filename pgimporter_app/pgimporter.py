06eaf7c9d5af687f4ec57787f34a195d542a0202
Carlos Silva <Carlos.TavaresdaSilva@werum.com>
2023-10-05 23:37:22 +0200

    allow to run without the dump folders expecified

--- pgimporter_app/pgimporter.py
+++ pgimporter_app/pgimporter.py
@@ -7,43 +7,47 @@ class Pgimporter:
         # Initialize an empty list to store file information
         file_info_list = []
 
-        # Iterate over the files in the specified folder
-        for filename in os.listdir(folder_path):
-            file_path = os.path.join(folder_path, filename)
+        try:
+            # Iterate over the files in the specified folder
+            for filename in os.listdir(folder_path):
+                file_path = os.path.join(folder_path, filename)
 
-            # Check if the item is a file (not a directory)
-            if os.path.isfile(file_path):
-                
-                file_type_bs_color = " "
-                file_extension = " "
-                try:
-                    filename.split(".")[1]
-                except:
-                    file_type_bs_color = "secondary"
-                    file_extension = "Other"
-                else:
-                    file_type_bs_color = "info" if filename.split(".")[1] == "sql" else "primary"
-                    file_extension = filename.split(".")[1]
-                
-                file_type = '<span class="badge bg-'+ file_type_bs_color +'">'+ file_extension +'</span>'
-                    
-                # Get the size of the file in bytes
-                file_size = os.path.getsize(file_path)
-                
-                # Extract the creation timestamp (ctime) from the file_stat
-                creation_timestamp = os.stat(file_path).st_ctime
+                # Check if the item is a file (not a directory)
+                if os.path.isfile(file_path):
 
-                # Convert the timestamp to a human-readable date format
-                file_creation_date = datetime.datetime.fromtimestamp(creation_timestamp).strftime('%Y-%m-%d %H:%M:%S')
+                    file_type_bs_color = " "
+                    file_extension = " "
+                    try:
+                        filename.split(".")[1]
+                    except:
+                        file_type_bs_color = "secondary"
+                        file_extension = "Other"
+                    else:
+                        file_type_bs_color = "info" if filename.split(".")[1] == "sql" else "primary"
+                        file_extension = filename.split(".")[1]
 
-                # Append the filename and its size to the list
-                file_info_list.append((file_type, filename, file_size, file_creation_date))
+                    file_type = '<span class="badge bg-'+ file_type_bs_color +'">'+ file_extension +'</span>'
 
+                    # Get the size of the file in bytes
+                    file_size = os.path.getsize(file_path)
+
+                    # Extract the creation timestamp (ctime) from the file_stat
+                    creation_timestamp = os.stat(file_path).st_ctime
+
+                    # Convert the timestamp to a human-readable date format
+                    file_creation_date = datetime.datetime.fromtimestamp(creation_timestamp).strftime('%Y-%m-%d %H:%M:%S')
+
+                    # Append the filename and its size to the list
+                    file_info_list.append((file_type, filename, file_size, file_creation_date))
+            return file_info_list
+        except FileNotFoundError as ex:
+            print("No dir or file discovered")
+            return file_info_list
         # Now, file_info_list contains tuples of (filename, file_size)
         # You can use this list to access the file names and their sizes
         #for filename, file_size in file_info_list:
         #    print(f"File: {filename}, Size: {file_size} bytes")
-        return file_info_list
+        
     
     @staticmethod
     def import_dump(dump_filename):
