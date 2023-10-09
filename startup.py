dc34aa311bef4aeb09f8deeba9f1ebc99976cde4
Carlos Silva <sabioes@gmail.com>
2023-10-10 00:09:30 +0200

    clean used py files

--- startup.py
+++ startup.py
@@ -9,4 +9,4 @@ startup.py, that just imports the app object. You can then just specify
 startup:app in the Gunicorn command.
 """
 
-from pgimporter_app.webapp import app
+from pgimporter_app.app import views
