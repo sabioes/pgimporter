221730c15be3ef94f5fa59a7fba634cd84021f73
Carlos Silva <sabioes@gmail.com>
2024-11-06 00:26:53 +0100

    configuration database remote import

--- pgimporter_app/config.py
+++ pgimporter_app/config.py
@@ -13,4 +13,11 @@ class Config:
     IMPORT_DUMP_PATH = os.getenv('IMPORT_DUMP_PATH', '/u02/pgbackup/db/postgres/import/')
     IMPORT_LOGS_PATH = os.getenv('IMPORT_LOGS_PATH', '/u02/pgbackup/db/postgres/import_logs/')
     EXPORT_DUMP_PATH = os.getenv('EXPORT_DUMP_PATH', '/u02/pgbackup/db/postgres/dumps/')
+
+    # Database connection configuration
+    DB_HOSTNAME = ""
+    DB_PORT = ""
+    DB_DATABASE = ""
+    DB_USERNAME = ""
+    DB_PASSWORD = os.getenv('PGPASSWORD', '')
     
